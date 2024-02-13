1fa2ba4e4a4b041429da8e6e173b84ade94ec363
Crowdbotics <team@crowdbotics.com>
2024-02-13 16:20:23 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "solar-performance-46302",
+  url: "https://solar-performance-46302.botics.co",
+  api: "https://solar-performance-46302.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
